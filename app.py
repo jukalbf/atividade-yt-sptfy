1870c0c847d16c67fd4998401cc51a8f84532914
jukalbf <lgbr212@gmail.com>
2025-04-29 09:53:48 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+load_dotenv()
+
+client_id = os.getenv("SPOTIFY_ID")
+client_secret = os.getenv("SPOTIFY_SECRET")
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+artist = "https://open.spotify.com/artist/3YQKmKGau1PzlVlkL1iodx?si=G12ImuQzQjyShV1150Tp1g"
+
+result = sp.artist_albums(artist_id=artist, album_type="album")
+
+# album = result.
+
+print(result)
